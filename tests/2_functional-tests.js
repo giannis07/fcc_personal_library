4a3ae24b947322f10d5873e5e29fcc74fb6c1e98
giannis07 <136395964+giannis07@users.noreply.github.com>
2024-09-14 01:54:38 +0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,3 @@
-/*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       
-*/
 
 const chaiHttp = require('chai-http');
 const chai = require('chai');
